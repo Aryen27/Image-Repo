6e5d165ea89844717cffc74d14a7f42386f0af22
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-09 16:04:17 +0530

    Implemented POST/signup route to create a new user in the DB

---
+++ backend/db/users.sql
@@ -0,0 +1,3 @@
+use classwork;
+
+create table users(uid int not null primary key auto_increment, name varchar(30) , email varchar(255) not null, password varchar(255) not null, image varhcar(255));
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
+import bcrypt from 'bcrypt'
 
 const app = express();
 app.use(express.json());
@@ -23,7 +24,9 @@ app.post("/login", async (req, res) => {
   try {
     // insert into users(name, email, password) values('Aryan', 'abc@gmail.com' , 'abc');
     const [results, fields] = await connection.query('SELECT * FROM users WHERE email=?', [email]);
-    console.log(results[0]);
+    if (results.length == 0) {
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
     storedUser= results[0];
     console.log(storedUser);
   } catch (err) {
@@ -32,10 +35,28 @@ app.post("/login", async (req, res) => {
   res.status(200).json({ success: true, message: `Welcome ${storedUser.name}` ,data: storedUser})
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const { name, email, password } = req.body;
   /*
   1. Check if user already exists: if exists alert
-  2. 
+  2. Hash password
+  3. Add user to db w/ hashed password
+  4.Add error handling for DB queries
   */
+  const [results, fields] = await connection.query('SELECT * FROM users WHERE email=?', [email]);
+  if (results.length != 0) {
+    return res.status(409).json({ success: false, message: 'User already exists' });
+  }
+  
+  // Encrypting password
+  const saltRounds = 10;
+  const hashedPass= await bcrypt.hash(password, saltRounds);
+  
+  try { 
+    const [results, fields] = await connection.query('INSERT INTO users(name, email, password) VALUES(?,?,?)', [name, email, hashedPass]);
+    return res.status(200).json({ success: true, message: 'Account has been created successfully', results});
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err });
+  }
+
 });
