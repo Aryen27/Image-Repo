f61af20f1f58817b1f8e51a6a092ac0544e2d13a
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-10 15:57:58 +0530

    Added styles to login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Login() {
   return (
-
+    <div className='d-flex flex-column mx-auto my-4 py-4 gap-4 w-75'>
+        <h3 className='mx-auto'>Login</h3>
+      <div className='d-flex flex-column'>
+      <label className='label-for'>Email</label>
+      <input class="form-control" type="text" placeholder="abc@gmail.com" aria-label="default input example"/>
+      </div>
+      <div className='d-flex flex-column'>
+      <label className='label-for'>Password</label>
+      <input class="form-control" type="text" placeholder="Your Password" aria-label="default input example"/>
+      </div>
+      <button className='btn btn-success w-25 mx-auto'>Submit</button>
+    </div>
   )
 }
 
