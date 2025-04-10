69b52d5a411de28aa21759399563baa119ed8203
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-10 15:57:14 +0530

    Reinitialized project with bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,42 @@
-import { useState } from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import AddPhoto from './pages/AddPhoto';
+import PhotosList from './pages/PhotosList';
+import Home from './pages/Home';
 
+const router = createBrowserRouter([  
+  {
+    path: '/',
+    element: <Home/>,
+    children: [
+      {
+        path: "login",
+        element:<Login/>
+        },
+        {
+          path: "signup",
+          element: <SignUp/>
+        },
+        {
+          path: 'photo/add',
+          element: <AddPhoto/>
+          
+      },
+      {
+        path: 'photo',
+        element: <PhotosList/>
+        },
+    ],
+  },
+]);
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+    <RouterProvider router={router}/>
+    </div>
   )
 }
 
