3cbf743049cc2348d2af1a0ff5f176d030406214
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-10 15:58:39 +0530

    Added styles to signup  page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 function SignUp() {
   return (
+<div className='d-flex flex-column mx-auto my-4 py-4 gap-4 w-75'>
+    <h3 className='mx-auto'>Sign Up</h3>
+  <div className='d-flex flex-column'>
+      <label className='label-for'>Username</label>
+  <input class="form-control" type="text" placeholder="Name" aria-label="default input example"/>
+  </div>
+      <div className='d-flex flex-column'>
+      <label className='label-for'>Email</label>
+      <input class="form-control" type="text" placeholder="abc@gmail.com" aria-label="default input example"/>
+      </div>
+      <div className='d-flex flex-column'>
+      <label className='label-for'>Password</label>
+      <input class="form-control" type="text" placeholder="Your Password" aria-label="default input example"/>
+      </div>
 
+      <div className='d-flex flex-column'>
+      <label className='label-for'>Password</label>
+      <input class="form-control" type="text" placeholder="Confirm Password" aria-label="default input example"/>
+      </div>
+  <button className='btn btn-success w-25 mx-auto'>Submit</button>
+</div>
   )
 }
 
