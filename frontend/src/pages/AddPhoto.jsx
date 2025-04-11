6651031a947a6b066b9449c3c74ee68ae25f9fef
Aryan Rajeev <raghavaan27@gmail.com>
2025-04-11 15:21:19 +0530

    Added styles for form to upload photos

--- frontend/src/pages/AddPhoto.jsx
+++ frontend/src/pages/AddPhoto.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function AddPhoto() {
   return (
-    <div>AddPhoto</div>
+    <div className='d-flex justify-content-center align-items-center my-5 py-5'>
+      <form method='POST' className='d-flex flex-column justify-content-center align-items-center gap-4'>
+        <div className='d-flex flex-column align-it'>
+        <label>Description</label>
+        <input type='text'id='about' className='form-control'/>
+        </div>
+        
+        <div className='d-flex flex-column justify-content-center'>
+        <label>Upload Image</label>
+        <input type='file' id='pic' required className='form-control'/>
+        </div>
+        <button className='btn btn-success'>Submit</button>
+      </form>
+    </div>
   )
 }
 
